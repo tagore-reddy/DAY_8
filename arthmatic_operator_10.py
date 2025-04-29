51bb5bf07a7530dc25a749e4b119fcc498e35b34
tagore-reddy <tagorereddykotala@gmail.com>
2025-04-29 13:01:23 +0530

    arthematic operators

---
+++ arthmatic_operator_10.py
@@ -0,0 +1,6 @@
+a=5
+b=2
+c=a//b
+print("value of A :",a)
+print("value of B :",b)
+print("value of C :",c)                                                                           
---
+++ arthmatic_operator_9.py
@@ -0,0 +1,6 @@
+a=4
+b=2
+c=a//b
+print("value of A :",a)
+print("value of B :",b)
+print("value of C :",c)                                                                           
